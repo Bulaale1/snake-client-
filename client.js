de232ae90aba1f0e6508e5663278b2b9de63d031
Yasin Hussein <bulaale116@gmail.com>
2024-01-17 15:41:02 -0700

    snakeClient initials

--- client.js
+++ client.js
@@ -8,9 +8,14 @@ const connect = function() {
     host: '165.227.47.243',// IP address here,
     port : 50541 // PORT number here,
   });
-  conn.on("connect", (data) => {
+  conn.on("data", (data) => {
     // code that does something when the connection is first established
     console.log('server says :',data);
+  });
+  conn.on('connect',()=>{
+    console.log('Connected successfully');
+    conn.write("Name:YMH");
+
   });
 
   // interpret incoming data as text
