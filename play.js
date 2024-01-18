71cb4597473a55d354fa05c74534c34b0f7ef1a4
Yasin Hussein <bulaale116@gmail.com>
2024-01-17 18:14:01 -0700

    WASD Movemen challenge was implemented

--- play.js
+++ play.js
@@ -3,5 +3,6 @@ const { connect } = require("./client");
 const { setupInput } = require("./input");
 
 console.log("Connecting ...");
-setupInput();
-connect();
+
+let con = connect();
+setupInput(con);
